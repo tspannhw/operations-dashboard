1ea00a123a58c306be5341c378cf6c6c675b6306
Timothy Spann <tspann@hortonworks.com>
2018-10-16 16:33:46 -0400

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# Operations Dashboard
+
+# Apache Hive 3 / HDP 3 / NiFi 3 / Spring Boot
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Xms512m -Xmx2048m -Dhdp.version=3.0.0 -Djava.net.preferIPv4Stack=true -jar target/operations-0.0.1-SNAPSHOT.jar
